bafb25b64c412675b9256f3af05d3795d445ee6a
Arsal Kaleem <m.arsal.kaleem@gmail.com>
2026-04-11 00:43:53 +0500

    Build the tours overview page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,11 +1,22 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  const tours = await Tour.find();
+  // 1) Get tour data from collection
+
+  // 2) Build template
+
+  // 3) Render that template using
+
   res.status(200).render('overview', {
     title: 'All tours',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
-  res.status(200).render('overview', {
+  res.status(200).render('tour', {
     title: 'The Forest Hiker',
   });
 };
